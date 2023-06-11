268d584c2620c4ac52945283338b5eba0a482b7d
dasoib <dasojib49@gmail.comm>
2023-06-12 00:28:26 +0600

    jwt initial

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -8,6 +9,21 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({error: true, message: 'Unauthorized access'})
+  }
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res.status(401).send({error:true, message: 'Unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 
@@ -34,6 +50,12 @@ async function run() {
     const reviewsCollection = client.db('sportsDb').collection('reviews')
     const classesCollection = client.db('sportsDb').collection('classes')
 
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'})
+      res.send({token})
+    })
 
     app.get('/users', async (req, res) => {
       const result = await usersCollection.find().toArray()
