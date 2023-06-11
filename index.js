68772e7af119f0d6eaf697507daaa59f49524e29
dasoib <dasojib49@gmail.comm>
2023-06-11 22:14:27 +0600

    admin make done

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d2bgkkq.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -60,6 +60,20 @@ async function run() {
           const result = await reviewsCollection.find().toArray()
           res.send(result)
       })
+
+
+    app.patch('users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id)}
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc)
+      res.send(result);
+      })
+
 
       //classes collection
     app.post('/classes', async (req, res) => {
